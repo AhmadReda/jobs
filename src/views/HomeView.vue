cf32de1c31cfd3ffb9ec97f2d39c65422c1ad42c
AhmadReda <ahmedreda.abdelsattar@gmail.com>
2023-08-07 11:40:23 +0300

    Setup Vue Router and Jobs Page

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed quis non aut commodi perferendis facere fugiat vitae sapiente, sequi ut ab illum eum delectus alias numquam! Rerum commodi sunt, recusandae iste, consectetur itaque sapiente cum at sed voluptate repudiandae hic pariatur beatae laboriosam natus quisquam enim rem esse animi impedit. Magnam tempore commodi quidem adipisci velit temporibus, sit ipsum molestiae blanditiis facilis recusandae ratione unde possimus consequuntur voluptates aut veritatis aliquam perspiciatis. Debitis laboriosam ex fuga sequi aut quia doloribus delectus porro deserunt numquam sed tempora animi, nobis nam perferendis mollitia dolor eius voluptatibus vero molestias exercitationem? Quam, aspernatur fuga?</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed quis non aut commodi perferendis facere fugiat vitae sapiente, sequi ut ab illum eum delectus alias numquam! Rerum commodi sunt, recusandae iste, consectetur itaque sapiente cum at sed voluptate repudiandae hic pariatur beatae laboriosam natus quisquam enim rem esse animi impedit. Magnam tempore commodi quidem adipisci velit temporibus, sit ipsum molestiae blanditiis facilis recusandae ratione unde possimus consequuntur voluptates aut veritatis aliquam perspiciatis. Debitis laboriosam ex fuga sequi aut quia doloribus delectus porro deserunt numquam sed tempora animi, nobis nam perferendis mollitia dolor eius voluptatibus vero molestias exercitationem? Quam, aspernatur fuga?</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeView',
+  components: {}
+}
+</script>
